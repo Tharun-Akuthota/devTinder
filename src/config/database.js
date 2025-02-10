a188707ec452ba40118a79a75d25de2cc923af15
Tharun Akuthota <tharunakuthota@gmail.com>
2025-02-10 12:38:09 +0530

    Solve issues with database

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   await mongoose.connect(
-    "mongodb+srv://tharunakuthota:aLIdi925be1WWHML@cluser0.ijruy.mongodb.net/devTinder"
+    "mongodb+srv://tharunakuthota:HpYTcegEQoYyUorx@cluser0.ijruy.mongodb.net/devTinder"
   );
 };
 
